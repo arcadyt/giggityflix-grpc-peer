6525cfd26e965a04398391b388387a07344c50dd
Arcady Trembovler <arcadyt@gmail.com>
2025-05-14 15:53:45 +0300

    version 0.1.0 cont.

--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-GiggityFlix gRPC Shared Module
-
-Provides shared gRPC/Protobuf interface definitions for communication
-between peer and edge services in the GiggityFlix ecosystem.
-"""
-
-__version__ = "0.1.0"
