7d171da95d0d68a7ce005c0fcf293a854c699071
Arcady Trembovler <arcadyt@gmail.com>
2025-05-15 11:44:39 +0300

    init fix

---
+++ src/giggityflix_grpc_peer/__init__.py
@@ -0,0 +1,7 @@
+# Re-export the generated modules
+from giggityflix_grpc_peer.generated.peer_edge.peer_edge_pb2 import *
+from giggityflix_grpc_peer.generated.peer_edge.peer_edge_pb2_grpc import *
+
+# Create module aliases for cleaner imports
+import giggityflix_grpc_peer.generated.peer_edge.peer_edge_pb2 as peer_edge_pb2
+import giggityflix_grpc_peer.generated.peer_edge.peer_edge_pb2_grpc as peer_edge_pb2_grpc
